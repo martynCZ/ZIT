dc018c74f352d121bc0b10381c2c2e8d0fe417e7
martynCZ <kozar.martin1307@gmail.com>
2025-11-24 16:04:50 +0100

    Sticky header, added button - PokeAPI generate, style changes

--- js/script.js
+++ js/script.js
@@ -106,3 +106,13 @@ const pokeApi = () =>{
 }
 pokeApi();
 
+window.addEventListener("scroll", () => {
+    const header = document.getElementById("header");
+
+    if (window.scrollY > 10) {
+        header.classList.add("bg-[var(--tmave-seda)]");
+    } else {
+        header.classList.remove("bg-[var(--tmave-seda)]");
+    }
+});
+
